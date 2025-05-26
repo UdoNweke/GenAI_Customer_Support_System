1523d0fe5960b2d45b63ad1262769e2d76528ad4
UdoNweke <unwek001@gmail.com>
2025-05-26 14:26:36 -0400

    read me updated with fastapi code

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 E-Commerce Chatbot with RAGS
+'''
+conda create -p env python=3.11 -y
+'''
+'''
+conda activate env
+'''
+'''
+uvicorn main:app --reload --port 8001
+'''
--- main.py
+++ main.py
@@ -2,4 +2,28 @@ import uvicorn
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from dotenv import load_dotenv
+
+#Configuration for FastAPI
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+#Allow CORS(optional for frontend)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+)
+
+#Define GET method
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    """
+    Render the chat Interface
+    """
+    return templates.TemplateResponse("chat.html", {"request": request})
